15958cba576b93459eeac4221dadb8ddcfa58e9d
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-07-26 16:21:37 +0300

    Added warning log in case of not configured asset for new cashin event

--- src/Lykke.Service.BalanceMismatches/Cqrs/CashinProjection.cs
+++ src/Lykke.Service.BalanceMismatches/Cqrs/CashinProjection.cs
@@ -1,4 +1,6 @@
-﻿using Lykke.Job.BlockchainCashinDetector.Contract.Events;
+﻿using Common.Log;
+using Lykke.Common.Log;
+using Lykke.Job.BlockchainCashinDetector.Contract.Events;
 using Lykke.Service.BalanceMismatches.Core.Services;
 using System.Threading.Tasks;
 
@@ -8,17 +10,25 @@ namespace Lykke.Service.BalanceMismatches.Cqrs
     {
         private readonly IHotWalletManager _hotWalletManager;
         private readonly IHotWalletBalancesManager _hotWalletBalancesManager;
+        private readonly ILog _log;
 
-        public CashinProjection(IHotWalletManager hotWalletManager, IHotWalletBalancesManager hotWalletBalancesManager)
+        public CashinProjection(
+            IHotWalletManager hotWalletManager,
+            IHotWalletBalancesManager hotWalletBalancesManager,
+            ILogFactory logFactory)
         {
             _hotWalletManager = hotWalletManager;
             _hotWalletBalancesManager = hotWalletBalancesManager;
+            _log = logFactory.CreateLog(this);
         }
 
         internal async Task Handle(CashinCompletedEvent evt)
         {
             if (!_hotWalletManager.IsAssetIdConfigured(evt.AssetId))
+            {
+                _log.Warning($"HotWallet is not configured for asset {evt.AssetId}");
                 return;
+            }
 
             await _hotWalletBalancesManager.UpdateAsync(evt.AssetId, evt.Amount);
         }
